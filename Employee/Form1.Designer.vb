e5abbcfe1eb1d7d5292548e63de127560ab29f77
Zohrehgha <37638793+Zohrehgha@users.noreply.github.com>
2018-04-27 13:29:32 -0700

    Add files via upload

---
+++ Employee/Form1.Designer.vb
@@ -0,0 +1,361 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte), True)
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1772, 984)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.BackColor = System.Drawing.Color.DarkGray
+        Me.TabPage1.Controls.Add(Me.GroupBox1)
+        Me.TabPage1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.125!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TabPage1.Location = New System.Drawing.Point(8, 64)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(1756, 912)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Login"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.DarkSeaGreen
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(128, 60)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(829, 528)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(435, 393)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(195, 76)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Cancel"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(184, 393)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(195, 76)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Login"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(109, 258)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(183, 42)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(109, 123)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(190, 42)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Username"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(347, 248)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(292, 63)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(347, 129)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(292, 58)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TabPage2
+        '
+        Me.TabPage2.BackColor = System.Drawing.Color.DarkSeaGreen
+        Me.TabPage2.Controls.Add(Me.DataGridView1)
+        Me.TabPage2.Controls.Add(Me.DateTimePicker1)
+        Me.TabPage2.Controls.Add(Me.Label7)
+        Me.TabPage2.Controls.Add(Me.Button5)
+        Me.TabPage2.Controls.Add(Me.Button6)
+        Me.TabPage2.Controls.Add(Me.Button3)
+        Me.TabPage2.Controls.Add(Me.Button4)
+        Me.TabPage2.Controls.Add(Me.TextBox6)
+        Me.TabPage2.Controls.Add(Me.TextBox5)
+        Me.TabPage2.Controls.Add(Me.TextBox4)
+        Me.TabPage2.Controls.Add(Me.TextBox3)
+        Me.TabPage2.Controls.Add(Me.Label6)
+        Me.TabPage2.Controls.Add(Me.Label5)
+        Me.TabPage2.Controls.Add(Me.Label4)
+        Me.TabPage2.Controls.Add(Me.Label3)
+        Me.TabPage2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.125!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TabPage2.Location = New System.Drawing.Point(8, 64)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(1756, 912)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Employee"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(816, 92)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 33
+        Me.DataGridView1.Size = New System.Drawing.Size(877, 413)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker1.Location = New System.Drawing.Point(291, 456)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(410, 49)
+        Me.DateTimePicker1.TabIndex = 33
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(47, 456)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(215, 42)
+        Me.Label7.TabIndex = 32
+        Me.Label7.Text = "Hiring Date"
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(395, 736)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(227, 61)
+        Me.Button5.TabIndex = 31
+        Me.Button5.Text = "Delete"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.Location = New System.Drawing.Point(125, 736)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(247, 61)
+        Me.Button6.TabIndex = 30
+        Me.Button6.Text = "Search"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(395, 644)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(227, 61)
+        Me.Button3.TabIndex = 29
+        Me.Button3.Text = "Modify"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(125, 644)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(247, 61)
+        Me.Button4.TabIndex = 28
+        Me.Button4.Text = "Add New"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(291, 364)
+        Me.TextBox6.Multiline = True
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(118, 68)
+        Me.TextBox6.TabIndex = 27
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(291, 278)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(410, 66)
+        Me.TextBox5.TabIndex = 26
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(291, 198)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(410, 64)
+        Me.TextBox4.TabIndex = 25
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(291, 111)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(118, 64)
+        Me.TextBox3.TabIndex = 24
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(47, 381)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(88, 42)
+        Me.Label6.TabIndex = 23
+        Me.Label6.Text = "Age"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(56, 302)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(176, 42)
+        Me.Label5.TabIndex = 22
+        Me.Label5.Text = "Surname"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(67, 211)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(121, 42)
+        Me.Label4.TabIndex = 21
+        Me.Label4.Text = "Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.875!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(47, 124)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(241, 42)
+        Me.Label3.TabIndex = 20
+        Me.Label3.Text = "Employee ID"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1803, 1049)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ Employee/Form1.vb
@@ -0,0 +1,184 @@
+﻿Public Class Form1
+    'connect to database access
+    Dim conn As New ADODB.Connection
+    Dim rs As New ADODB.Recordset    'recordset for security table
+    ' connect to sql database
+    Dim cn As New OleDb.OleDbConnection
+    Dim da As OleDb.OleDbDataAdapter
+    Dim ds As New DataSet
+    Dim Maxrecord As Integer
+    Dim CurrentRow As Integer
+    'create an array of datarows 
+    ' so the result of select method can live in this array
+    Dim FoundRows() As Data.DataRow
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'setting up the ADO objects(access)
+        conn.Provider = "Microsoft.Jet.OleDB.4.0"
+        ' Setting up the jet DB driver (access) 
+        conn.ConnectionString = "C:\ITD\Term 3\Visual Basic.Net\assignment\Final\EmployeeDB.mdb"
+        conn.Open()
+        rs.Open("select * from securityTable", conn, ADODB.CursorTypeEnum.adOpenDynamic, ADODB.LockTypeEnum.adLockPessimistic)
+        'hiding tab control
+        TabControl1.TabPages.Remove(TabControl1.TabPages("tabpage2"))
+
+        'TODO: This line of code loads data into the sql table.
+        cn.ConnectionString = "Provider = SQLNCLI11.0;Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\ITD\Term 3\Visual Basic.Net\assignment\Final\Employee\Employee\EmployeeDatabase.mdf;Integrated Security = SSPI;database=thingy"
+        cn.Open()
+        'for sql table
+        Dim SqlStr As String
+        SqlStr = "Select * from EmployeeTable"
+        da = New OleDb.OleDbDataAdapter(SqlStr, cn)
+        da.Fill(ds, "EmployeeTable")
+        Maxrecord = ds.Tables("EmployeeTable").Rows.Count - 1
+        'connect dataGridView to EmployeeTable
+        DataGridView1.DataSource = ds.Tables(0)
+
+    End Sub
+    'verify clientID and Password by reading the info from the database 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "" Or TextBox2.Text = "" Then
+            MsgBox("Plz Fill Client ID and Password")
+        Else
+            Dim Criteria As String
+            Criteria = "userName = '" + TextBox1.Text + "'"
+            rs.MoveFirst()
+            rs.Find(Criteria)
+            If rs.EOF Then
+                MsgBox("This username does not exit")
+            Else
+                If (rs.Fields("userName").Value.ToString = TextBox1.Text And rs.Fields("Password").Value.ToString = TextBox2.Text) Then
+                    MessageBox.Show("Login succesfully")
+                    'access to tab control to user
+                    TabControl1.TabPages.Add(TabPage2)
+                    TabControl1.SelectedTab = TabControl1.TabPages("Employee")
+                    'TabControl1.TabPages.Remove(TabPage1)
+                    TextBox1.Clear()
+                    TextBox2.Clear()
+                Else
+                    MessageBox.Show("Invalid Account")
+                    TextBox1.Clear()
+                    TextBox2.Clear()
+                End If
+            End If
+        End If
+    End Sub
+    'adding new employee to sql database
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim Criteria As String
+        If TextBox3.Text = "" Then
+            MessageBox.Show("Please Enter an Employee ID to Add")
+            Exit Sub
+        End If
+        If TextBox4.Text = "" Or TextBox5.Text = "" Or TextBox6.Text = "" Then
+            MessageBox.Show("Incomplete Data, please fill all fields")
+            Exit Sub
+        End If
+        Criteria = "EmployeeID = '" + TextBox3.Text + "'"
+        FoundRows = ds.Tables("EmployeeTable").Select(Criteria)
+        If FoundRows.Length = 0 Then
+            Dim NewRow As DataRow = ds.Tables("EmployeeTable").NewRow()
+            'the next line is needed so VB Can execute the SQL statement 
+            Dim Cb As New OleDb.OleDbCommandBuilder(da)
+            NewRow.Item("EmployeeID") = Convert.ToInt32(TextBox3.Text)
+            NewRow.Item("Name") = TextBox4.Text
+            NewRow.Item("SurName") = TextBox5.Text
+            NewRow.Item("Age") = TextBox6.Text
+            NewRow.Item("HiringDate") = Convert.ToDateTime(DateTimePicker1.Text)
+
+            ds.Tables.Item("EmployeeTable").Rows.Add(NewRow)
+            da.Update(ds, "EmployeeTable")
+            Maxrecord = Maxrecord + 1
+            MessageBox.Show("Record Added succesfully!!!")
+            Call ClearForm()
+        Else
+            MessageBox.Show("Duplicate Record!!!, Try another Employee ID")
+        End If
+    End Sub
+    Public Sub ClearForm()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox5.Clear()
+        TextBox6.Clear()
+    End Sub
+    'modifying data into sql table
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim Strtofind As String
+        If TextBox3.Text = "" Then
+            MessageBox.Show("Please Enter a Valid Employee ID to find")
+            Exit Sub
+        End If
+        Strtofind = "EmployeeID = '" + TextBox3.Text + "'"
+        FoundRows = ds.Tables("EmployeeTable").Select(Strtofind)
+        Dim RowIndex As Integer
+        If FoundRows.Length = 0 Then
+            MessageBox.Show("Record Not Found, try again")
+        Else
+            'save the data 
+            Dim Cb As New OleDb.OleDbCommandBuilder(da)
+            RowIndex = ds.Tables("EmployeeTable").Rows.IndexOf(FoundRows(0))
+            ds.Tables("EmployeeTable").Rows(RowIndex).Item(0) = TextBox3.Text
+            ds.Tables("EmployeeTable").Rows(RowIndex).Item(1) = TextBox4.Text
+            ds.Tables("EmployeeTable").Rows(RowIndex).Item(2) = TextBox5.Text
+            ds.Tables("EmployeeTable").Rows(RowIndex).Item(3) = TextBox6.Text
+            ds.Tables("EmployeeTable").Rows(RowIndex).Item(4) = DateTimePicker1.Text
+            da.Update(ds, "EmployeeTable")
+            MessageBox.Show("Record Modified Succesfully")
+        End If
+    End Sub
+    'search by id
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim Criteria As String
+        If TextBox3.Text = "" Then
+            MessageBox.Show("Please Enter a Valid Employee ID to find")
+            Exit Sub ' = return
+        End If
+        Criteria = "EmployeeID = '" + TextBox3.Text + "'"
+        FoundRows = ds.Tables("EmployeeTable").Select(Criteria)
+        If FoundRows.Length = 0 Then 'if the array is empty
+            'which it means record not found 
+            MessageBox.Show("Record not found !!!")
+            Exit Sub
+        Else
+            'record has been found 
+            Dim RowIndex As Integer
+            RowIndex = ds.Tables("EmployeeTable").Rows.IndexOf(FoundRows(0))
+            Call ShowRecord(RowIndex)
+            CurrentRow = ds.Tables("EmployeeTable").Rows.IndexOf(FoundRows(0))
+        End If
+    End Sub
+    'Delete data from sql database
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim Strtofind As String
+        If TextBox3.Text = "" Then
+            MessageBox.Show("Please Enter a Valid Employee ID to delete")
+            Exit Sub
+        End If
+        Strtofind = "EmployeeID = '" + TextBox3.Text + "'"
+        FoundRows = ds.Tables("EmployeeTable").Select(Strtofind)
+        Dim RowIndex As Integer
+        If FoundRows.Length = 0 Then
+            MessageBox.Show("Record Not Found, try another ISBN")
+        Else
+            'Delete the data 
+            Dim Cb As New OleDb.OleDbCommandBuilder(da)
+            Dim result As Integer
+            RowIndex = ds.Tables("EmployeeTable").Rows.IndexOf(FoundRows(0))
+            Call ShowRecord(RowIndex)
+            result = MessageBox.Show("Are you Sure?", "Deleting Record", MessageBoxButtons.YesNo)
+            If result = vbYes Then
+                ds.Tables("EmployeeTable").Rows(RowIndex).Delete()
+                Call ClearForm()
+                MessageBox.Show("Record deleted Succesfully!!!")
+                Maxrecord = Maxrecord - 1
+            End If
+        End If
+    End Sub
+    'showing data from sql table into form
+    Public Sub ShowRecord(ByVal ThisRow As Integer)
+        TextBox3.Text = ds.Tables("EmployeeTable").Rows(ThisRow).Item(0)
+        TextBox4.Text = ds.Tables("EmployeeTable").Rows(ThisRow).Item(1)
+        TextBox5.Text = ds.Tables("EmployeeTable").Rows(ThisRow).Item(2)
+        TextBox6.Text = ds.Tables("EmployeeTable").Rows(ThisRow).Item(3)
+        DateTimePicker1.Text = ds.Tables("EmployeeTable").Rows(ThisRow).Item(4)
+    End Sub
+End Class
